58f99a04341cd8223f6b6dfb31ac1dbe0c0493e0
coffeed-cat <gwcattower@gmail.com>
2021-07-17 15:15:57 +0900

    회원가입 인증 미들웨어 수정

--- middlewares/auth-Middleware.js
+++ middlewares/auth-Middleware.js
@@ -6,7 +6,7 @@ module.exports = async (req, res, next) => {
     const { authorization } = req.headers;
     const [tokenType, tokenValue] = authorization.split(' ');
 
-    if (tokenType !== 'Bearer') {
+    if (!authorization || tokenType !== 'Bearer') {
         res.status(401).send({
             errorMessage: '로그인 후 사용하세요',
         });
--- routers/user.js
+++ routers/user.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const registerValidator = require('../middlewares/registerValidator');
+const jwt = require('jsonwebtoken');
+const SHA256 = require('crypto-js/sha256');
+
+// 회원가입
+router.post('/register', registerValidator, async (req, res) => {
+    const { id, password, nickname } = req.body;
+});
+
+// 로그인
+router.post('/login', async (req, res) => {});
 
 module.exports = router;
