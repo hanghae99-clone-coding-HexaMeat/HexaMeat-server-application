09c4aa740953292d3373e019e0b89fe31acbf71a
최완호 <dhksgh9506@icloud.com>
2021-07-17 17:51:21 +0900

    Add auth middleWare to mainRouter

--- routers/index.js
+++ routers/index.js
@@ -3,9 +3,10 @@ const userRouter = require('./user');
 const productRouter = require('./product');
 const cartRouter = require('./cart');
 const router = express.Router();
+const auth = require('../middlewares/auth-Middleware')
 
 router.use('/user', userRouter);
 router.use('/products', productRouter);
-router.use('/cart', cartRouter);
+router.use('/cart', auth, cartRouter);
 
 module.exports = router;
