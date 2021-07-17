d5d6661e0fba4166917744aa56d10f9542501189
coffeed-cat <gwcattower@gmail.com>
2021-07-17 16:57:02 +0900

    cartRouter 틀

--- routers/cart.js
+++ routers/cart.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+//조회
+router.get('/', async (req, res) => {});
+
+//추가
+router.post('/', async (req, res) => {});
+
+//수정
+router.patch('/', async (req, res) => {});
+
+//삭제
+router.delete('/', async (req, res) => {});
+
 module.exports = router;
