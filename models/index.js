3c6cb16d59c3cd51baf478e3ddf77c21b02bb656
coffeed-cat <gwcattower@gmail.com>
2021-07-17 13:18:28 +0900

    크롤링 어려워

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// mongodb://test:test@localhost:27017/awesomeblog?authSource=admin
+
+const connect = () => {
+  mongoose
+    .connect("mongodb://localhost:27017/hexameattest", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      ignoreUndefined: true,
+    })
+    .catch((err) => console.log(err));
+};
+
+mongoose.connection.on("error", (err) => {
+  console.error("몽고디비 연결 에러", err);
+});
+
+module.exports = connect;
---
+++ models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  imgArr: {
+    type: Array,
+    required: true,
+  },
+  detailDesc: {
+    type: String,
+    required: true,
+  },
+});
+
+productSchema.virtual("productId").get(function () {
+  return this._id.toHexString();
+});
+
+productSchema.set("toJSON", {
+  virtuals: true,
+});
+
+module.exports = mongoose.model("Product", productSchema);
