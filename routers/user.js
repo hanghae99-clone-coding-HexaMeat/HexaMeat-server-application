2532caee0f88ccf6aa81197fd4c1d438ab4673d6
coffeed-cat <gwcattower@gmail.com>
2021-07-19 15:06:29 +0900

    add encryptPassword function

--- routers/user.js
+++ routers/user.js
@@ -6,28 +6,32 @@ const jwt = require('jsonwebtoken');
 const SHA256 = require('crypto-js/sha256');
 const SECRET_KEY = require('../config/secretKey');
 
-// 회원가입 : 성공 시 즉시 토큰 던져줌
+function encryptPassword(password) {
+    return SHA256(password).toString();
+}
+
 router.post('/register', registerValidator, async (req, res) => {
     let { id, password, nickname } = req.body;
 
-    password = SHA256(password).toString(); // 패스워드 암호화
+    const encryptedPassword = encryptPassword(password);
 
-    await User.create({ id, password, nickname });
+    await User.create({ id, password: encryptedPassword, nickname });
 
-    const token = jwt.sign({ id: id }, SECRET_KEY); // 추상화할 때 Token 생성하는 함수 따로 만들 수 있을 듯
-
-    res.status(201).send({ token });
+    res.status(201).send({});
 });
 
 // 로그인
 router.post('/login', async (req, res) => {
     let { id, password } = req.body;
 
-    password = SHA256(password).toString();
+    const encryptedPassword = encryptPassword(password);
 
-    const user = await User.findOne({ id, password });
+    const isThisUserExist = await User.findOne({
+        id,
+        password: encryptedPassword,
+    });
 
-    if (!user) {
+    if (!isThisUserExist) {
         res.status(401).send({
             errorMessage: 'ID 또는 비밀번호를 다시 한번 확인해주세요.',
         });
