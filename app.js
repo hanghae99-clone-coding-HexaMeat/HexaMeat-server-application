76bb81e14982d79429dd2c4492d0c819c8c5af4f
coffeed-cat <gwcattower@gmail.com>
2021-07-19 13:34:27 +0900

    cors 적용

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const connect = require('./config/index');
 const mainRouter = require('./routers/index');
+const cors = require('cors');
 
 connect();
 
-app.use('/', express.urlencoded({ extended: false }),
-    express.json(), mainRouter);
+app.use(cors());
+
+app.use(
+    '/',
+    express.urlencoded({ extended: false }),
+    express.json(),
+    mainRouter
+);
 
 app.listen(3000, () => {
     console.log('Hi!');
