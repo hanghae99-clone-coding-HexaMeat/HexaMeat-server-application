d3741e7bcf7f0edcb14827756ab652056c7aa493
coffeed-cat <gwcattower@gmail.com>
2021-07-20 10:01:50 +0900

    장바구니 테스트 코드 추가

--- routers/cart.spec.js
+++ routers/cart.spec.js
@@ -9,6 +9,8 @@ const token =
 
 let userId = null;
 
+const productId = '60f58e3990396a1964b46c98';
+
 test('장바구니를 요청하면 물건리스트와 총 갯수를 반환받는다.', async () => {
     let response = await supertest(app)
         .get('/cart')
@@ -20,9 +22,7 @@ test('장바구니를 요청하면 물건리스트와 총 갯수를 반환받는
 
     await supertest(app)
         .post(
-            encodeURI(
-                '/cart?productId=60f4cfcbeaed353bf0263c61&productOption=두껍(24mm)'
-            )
+            encodeURI(`/cart?productId=${productId}&productOption=두껍(24mm)`)
         )
         .set('authorization', token)
         .send({
@@ -32,9 +32,123 @@ test('장바구니를 요청하면 물건리스트와 총 갯수를 반환받는
     response = await supertest(app).get('/cart').set('authorization', token);
 
     expect(response.status).toEqual(200);
-    expect(response.body.products[0].productId).toEqual(
-        '60f4cfcbeaed353bf0263c61'
+    expect(response.body.products[0].productId).toEqual(productId);
+    expect(response.body.cartCount).toEqual(1);
+});
+
+test('장바구니에 상품을 추가할 시 기존에 들어있던 상품이라면 갯수를 더한다.', async () => {
+    let response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=두껍(24mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 1,
+        });
+
+    expect(response.status).toEqual(200);
+
+    response = await supertest(app).get('/cart').set('authorization', token);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.products[0].productId).toEqual(productId);
+    expect(response.body.cartCount).toEqual(1);
+
+    response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=두껍(24mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 5,
+        });
+
+    expect(response.status).toEqual(200);
+
+    response = await supertest(app).get('/cart').set('authorization', token);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.products[0].quantity).toEqual(6);
+    expect(response.body.cartCount).toEqual(1);
+});
+
+test('같은 상품명이라도 옵션이 다르면 다른 상품으로서 장바구니에 추가된다.', async () => {
+    let response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=두껍(24mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 3,
+        });
+
+    expect(response.status).toEqual(200);
+
+    response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=보통(16mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 5,
+        });
+
+    expect(response.status).toEqual(200);
+
+    response = await supertest(app).get('/cart').set('authorization', token);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.products[0].quantity).toEqual(3);
+    expect(response.body.products[1].quantity).toEqual(5);
+    expect(response.body.cartCount).toEqual(2);
+});
+
+test('존재하지 않는 상품 또는 옵션은 장바구니에 추가할 수 없다.', async () => {
+    let response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=두껍(24mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 1,
+        });
+
+    expect(response.status).toEqual(200);
+
+    response = await supertest(app)
+        .post(
+            encodeURI(
+                '/cart?productId=60ee89ca73e8b33dec17a6bf&productOption=두껍(24mm)'
+            )
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 1,
+        });
+
+    expect(response.status).toEqual(400);
+    expect(response.body.errorMessage).toEqual(
+        '존재하지 않는 상품 또는 옵션입니다.'
     );
+
+    response = await supertest(app)
+        .post(
+            encodeURI(`/cart?productId=${productId}&productOption=안두껍(1mm)`)
+        )
+        .set('authorization', token)
+        .send({
+            quantity: 1,
+        });
+
+    expect(response.status).toEqual(400);
+    expect(response.body.errorMessage).toEqual(
+        '존재하지 않는 상품 또는 옵션입니다.'
+    );
+
+    response = await supertest(app).get('/cart').set('authorization', token);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.products[0].productId).toEqual(productId);
     expect(response.body.cartCount).toEqual(1);
 });
 
